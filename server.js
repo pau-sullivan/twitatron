26e16e7e9a91ab831a524795c9532439aa5bda75
unknown <paulas@Paulas-PC.lar2.mobilestreams.com>
2015-09-29 17:21:35 -0300

    sin msg

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // Load required packages
 var express = require('express');
+var compression = require('compression');
 
 // Create our Express application
 var app = express();
 
+// Load required packages
+// defaults the bytes threshold to 1024
+app.use(compression()); 
+
 // Add static middleware
-app.use(express.static(__dirname + '/public'));
+var oneDay = 86400000;
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+// Add jade view engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
 // Create our Express router
 var router = express.Router();
@@ -16,7 +26,7 @@ router.get('/', function(req, res) {
 });
 
 // Register all our routes
-//app.use(router);
+app.use(router);
 
 // Start the server
 app.listen(3000);
