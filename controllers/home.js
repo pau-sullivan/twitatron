60b5ae240b1514013ae8eada224b48d0915171ed
unknown <paulas@Paulas-PC.lar2.mobilestreams.com>
2015-09-29 17:45:57 -0300

    mes

---
+++ controllers/home.js
@@ -0,0 +1,6 @@
+exports.index = function(req, res) {
+  res.locals.ip = req.ip;
+  res.render('home');
+};
+
+
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // Load required packages
 var express = require('express');
 var compression = require('compression');
+var path = require('path');
+
+// Load controllers
+var homeController = require('./controllers/home');
 
 // Create our Express application
 var app = express();
@@ -11,19 +15,23 @@ app.use(compression());
 
 // Add static middleware
 var oneDay = 86400000;
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+//app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneDay }));
 
 // Add jade view engine
-app.set('views', __dirname + '/views');
+//app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Create our Express router
 var router = express.Router();
 
-// Initial dummy route for testing
-router.get('/', function(req, res) {
-  res.end('Twitatron');
-});
+// Landing page route
+//router.get('/', function(req, res) {
+//    res.locals.ip = req.ip;
+//    res.render('home');
+//});
+router.get('/', homeController.index);
 
 // Register all our routes
 app.use(router);
